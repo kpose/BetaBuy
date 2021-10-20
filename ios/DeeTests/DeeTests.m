1c0c1056a4a29cc3f27ce0f26b3aa980f40eb95c
kpose <ganihujude@gmail.com>
2021-10-20 11:57:22 +0100

    rename project

--- ios/DeeTests/DeeTests.m
+++ ios/DeeTests/DeeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BetabuyTests : XCTestCase
+@interface DeeTests : XCTestCase
 
 @end
 
-@implementation BetabuyTests
+@implementation DeeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
