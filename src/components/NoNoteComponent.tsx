2ca0ee8f4fd51761e42d000de1484888af1658ac
kpose <ganihujude@gmail.com>
2021-11-06 10:16:52 +0100

    Feature(Lottie animation): add empty lottie animation

--- src/components/NoNoteComponent.tsx
+++ src/components/NoNoteComponent.tsx
@@ -1,13 +1,17 @@
-import {fonts} from 'app/utils';
+import {fonts, hp, wp} from 'app/utils';
 import React from 'react';
 import {View, StyleSheet, ScrollView} from 'react-native';
 import {Text} from 'react-native-paper';
+import LottieView from 'lottie-react-native';
 
 const NoNoteComponent = () => {
   return (
     <ScrollView
       contentContainerStyle={styles.noNoteContainer}
       showsHorizontalScrollIndicator={false}>
+      <View style={{height: hp(40)}}>
+        <LottieView source={require('../assets/think.json')} autoPlay loop />
+      </View>
       <Text style={[fonts.title]}>Notes you add will appear here.</Text>
     </ScrollView>
   );
@@ -23,5 +27,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignSelf: 'center',
     justifyContent: 'center',
+  },
+  lottieStyle: {
+    height: hp(40),
+    width: wp(60),
   },
 });
